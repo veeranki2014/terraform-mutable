edfd2349b4ac2e2d00a0d20059584b050851e443
veeranki2014 <veeranki2014@gmail.com>
2022-09-13 22:17:42 +0530

    RoboShop Mutable Implementation.

--- vpc/route-tables.tf
+++ vpc/route-tables.tf
@@ -2,7 +2,7 @@ resource "aws_route_table" "public" {
   vpc_id                      = aws_vpc.main.id
 
   route {
-    cidr_block                = "0.0.0.0"
+    cidr_block                = "0.0.0.0/0"
     gateway_id                = aws_internet_gateway.igw.id
   }
 
@@ -15,7 +15,7 @@ resource "aws_route_table" "private" {
   vpc_id                      = aws_vpc.main.id
 
   route {
-    cidr_block                = "0.0.0.0"
+    cidr_block                = "0.0.0.0/0"
     gateway_id                = aws_nat_gateway.ngw.id
   }
 
