a0c65a98101e55a6b474c42b399080289ca2ac69
veeranki2014 <veeranki2014@gmail.com>
2022-09-13 21:52:05 +0530

    RoboShop Mutable Implementation.

---
+++ vpc/gateway.tf
@@ -0,0 +1,24 @@
+resource "aws_internet_gateway" "igw" {
+  vpc_id               = aws_vpc.main.id
+
+  tags                 = {
+    Name               = "${var.ENV}-igw"
+  }
+}
+
+resource "aws_eip" "ngw" {
+    vpc               = true
+}
+
+resource "aws_nat_gateway" "example" {
+  allocation_id         = aws_eip.ngw.id
+  subnet_id             = aws_subnet.private.*.id[0]
+
+  tags                  = {
+    Name                = "${var.ENV}-ngw"
+  }
+
+  # To ensure proper ordering, it is recommended to add an explicit dependency
+  # on the Internet Gateway for the VPC.
+  depends_on            = [aws_internet_gateway.igw]
+}
