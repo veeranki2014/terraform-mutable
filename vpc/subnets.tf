cd8bcfbd9c7be1f03c273703cccf9d25827a2ce0
veeranki2014 <veeranki2014@gmail.com>
2022-09-14 08:58:30 +0530

    RoboShop Mutable Implementation.

--- vpc/subnets.tf
+++ vpc/subnets.tf
@@ -9,6 +9,7 @@ resource "aws_subnet" "private" {
 }
 
 resource "aws_subnet" "public" {
+  depends_on          = [aws_vpc_ipv4_cidr_block_association.public_cidr]
   count               = length(var.PUBLIC_SUBNET_CIDRS)
   vpc_id              = aws_vpc.main.id
   cidr_block          = element(var.PUBLIC_SUBNET_CIDRS, count.index)
