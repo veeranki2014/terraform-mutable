292da69297f997076fa8b94f3e7ad60a76b0164c
veeranki2014 <veeranki2014@gmail.com>
2022-09-17 06:20:31 +0530

    RoboShop Mutable Implementation.

--- databases/vars.tf
+++ databases/vars.tf
@@ -1,4 +1,4 @@
 variable "ENV"                          {}
-variable "MANGO_INSTANCE_TYPE"          {}
+variable "MONGO_INSTANCE_TYPE"          {}
 variable "RABBITMQ_INSTANCE_TYPE"       {}
 variable "RDS_MYSQL_INSTANCE_TYPE"      {}
