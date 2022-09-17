cccb44b09c497fd96335cba31abf46baf9474647
veeranki2014 <veeranki2014@gmail.com>
2022-09-17 14:58:12 +0530

    RoboShop Mutable Implementation.

--- databases/env/dev.tfvars
+++ databases/env/dev.tfvars
@@ -1,6 +1,6 @@
 ENV                             = "dev"
 MONGO_INSTANCE_TYPE             = "t3.micro"
 RABBITMQ_INSTANCE_TYPE          = "t3.micro"
-//RDS_MYSQL_INSTANCE_TYPE         = "db.t3.micro"
+RDS_MYSQL_INSTANCE_TYPE         = "db.t3.micro"
 
 
---
+++ databases/mysql.tf
@@ -0,0 +1,54 @@
+resource "aws_db_instance" "mysql" {
+  allocated_storage               = 10
+  name                            = "terraformdefault"
+  engine                          = "mysql"
+  engine_version                  = "5.7"
+  instance_class                  = var.RDS_MYSQL_INSTANCE_TYPE
+  username                        = "admin"
+  password                        = "Roboshop123"
+  parameter_group_name            = aws_db_parameter_group.mysql.name
+  skip_final_snapshot             = true
+  db_subnet_group_name            = aws_db_subnet_group.mysql.name
+  identifier                      = "mysql-${var.ENV}"
+  vpc_security_group_ids          = [aws_security_group.allow_mysql.id]
+}
+
+resource "aws_db_parameter_group" "mysql" {
+  name                            = "pg-for-${var.ENV}-mysql"
+  family                          = "mysql5.7"
+}
+
+resource "aws_db_subnet_group" "mysql" {
+  name                            = "mysql-${var.ENV}-subnet-group"
+  subnet_ids                      = data.terraform_remote_state.vpc.outputs.PRIVATE_SUBNETS
+  tags                            = {
+    Name                          = "mysql-${var.ENV}-subnet-group"
+  }
+}
+
+resource "aws_security_group" "allow_mysql" {
+  name                          = "allow_mysql_${var.ENV}"
+  description                   = "allow_mysql_${var.ENV}"
+  vpc_id                        = data.terraform_remote_state.vpc.outputs.VPC_ID
+
+  ingress                       {
+    description               = "MYSQL"
+    from_port                 = 3306
+    to_port                   = 3306
+    protocol                  = "tcp"
+    cidr_blocks               = [data.terraform_remote_state.vpc.outputs.VPC_PRIVATE_CIDR, data.terraform_remote_state.vpc.outputs.DEFAULT_VPC_CIDR]
+  }
+
+  egress                        {
+    from_port                 = 0
+    to_port                   = 0
+    protocol                  = "-1"
+    cidr_blocks               = ["0.0.0.0/0"]
+  }
+
+  tags                          = {
+    Name                        = "allow_mysql_${var.ENV}"
+    Environment                 = var.ENV
+  }
+}
+
