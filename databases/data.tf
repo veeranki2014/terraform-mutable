fe4a483e490302a13957ce71175488e09a93798d
veeranki2014 <veeranki2014@gmail.com>
2022-09-17 13:44:01 +0530

    RoboShop Mutable Implementation.

--- databases/data.tf
+++ databases/data.tf
@@ -16,19 +16,3 @@ data "terraform_remote_state" "vpc" {
 }
 
 
-/*data "aws_ami" "ami" {
-  //most_recent      = true
-  //name_regex       = "^base-image"
-  name_regex       = "^Centos*"
-  owners           = ["973714476881"]
-  //owners           = ["self"]
-}
-
-data "terraform_remote_state" "vpc" {
-  backend               = "s3"
-  config                = {
-    bucket              = "terraform-roboshop"
-    key                 = "mutable/vpc/${var.ENV}/terraform.tfstate"
-    region              = "us-east-1"
-    }
-  }*/
